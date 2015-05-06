a2ff346ea3e522513d71eba2773c2ebfc347b80f
Erika Jonikaite <erikajon@hotmail.co.uk>
2015-05-06 22:42:19 +0100

    Got the display of the numbers working

--- atm-start_jQuery/atm.js
+++ atm-start_jQuery/atm.js
@@ -1,4 +1,38 @@
 $(document).ready(function(){
-  
+
+/************************
+CURRENT ACCOUNT
+************************/
+
+$('#deposit1').on('click', add1);
+
+function add1() {
+  var input = parseInt($('#amount1').val());
+  var initBalance1 = $('#balance1').text(); 
+  if (initBalance1 === '$0') { 
+    $('#balance1').text('$' + input); 
+  } else {
+    debugger;
+    input = parseInt(input);
+    initBalance1 = parseInt(initBalance1);
+    var finalBalance1 = input + initBalance1;
+    $('#balance1').text('$' + finalBalance1);
+  }
+
+} 
+
+/************************
+SAVINGS ACCOUNT
+************************/
+
+  $('#deposit2').on('click', add2);
+
+
+  function add2() {
+    var input = $('#amount2').val();
+    $('#balance2').text('$' + input);
+  };
+
+
 })
 
