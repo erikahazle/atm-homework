8c140356ccbd73b893e8b20003fdd24726f57ad4
Erika Jonikaite <erikajon@hotmail.co.uk>
2015-05-06 23:16:56 +0100

    Added functions to take in additional deposits

--- atm-start_jQuery/atm.js
+++ atm-start_jQuery/atm.js
@@ -7,19 +7,35 @@ CURRENT ACCOUNT
 $('#deposit1').on('click', add1);
 
 function add1() {
-  var input = parseInt($('#amount1').val());
+  var input1 = parseInt($('#amount1').val());
   var initBalance1 = $('#balance1').text(); 
   if (initBalance1 === '$0') { 
-    $('#balance1').text('$' + input); 
+    $('#balance1').text('$' + input1); 
   } else {
-    debugger;
-    input = parseInt(input);
-    initBalance1 = parseInt(initBalance1);
-    var finalBalance1 = input + initBalance1;
+    var array1 = initBalance1.split('');
+    array1.shift();
+    initBalance1 = parseInt(array1.join(''));
+    var finalBalance1 = input1 + initBalance1;
     $('#balance1').text('$' + finalBalance1);
   }
-
 } 
+
+$('#withdraw1').on('click', withdraw1);
+
+function withdraw1() {
+  var input1 = parseInt($('#amount1').val());
+  var initBalance1 = $('#balance1').text(); 
+
+// Handling #balance1 value
+
+  var array1 = initBalance1.split('');
+  array1.shift();
+  initBalance1 = parseInt(array1.join(''));
+
+
+  var finalBalance1 = initBalance1 - input1;
+  $('#balance1').text('$' + finalBalance1);
+}
 
 /************************
 SAVINGS ACCOUNT
